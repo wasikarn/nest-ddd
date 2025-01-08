47e3c6a3b05bd36931d12f53eafcb6407cb97df3
BIG <bigguitarsolo@gmail.com>
2025-01-08 23:10:58 +0700

    Refactor camper repository for improved readability.
    Simplified methods by introducing `mapToCamperDto` and renamed variables
     for clarity. Enhanced code structure with detailed function
     documentation and consistent naming conventions. This improves
     maintainability and code comprehension.

--- src/campers/db/camper-dto.repository.ts
+++ src/campers/db/camper-dto.repository.ts
@@ -16,35 +16,62 @@ export class CamperDtoRepository {
     private readonly camperModel: Model<CamperDocument>,
   ) {}
 
+  /**
+   * Retrieves all camper documents from the database and maps them to CamperDto objects.
+   *
+   * @return {Promise<CamperDto[]>} A promise that resolves to an array of CamperDto objects.
+   */
   async findAll(): Promise<CamperDto[]> {
-    const campers: CamperLeanDocument[] = await this.camperModel
+    const camperDocuments: CamperLeanDocument[] = await this.camperModel
       .find()
       .lean<CamperLeanDocument[]>();
 
-    return campers.map((camper: CamperLeanDocument): CamperDto => {
-      const allergiesLower: string[] = this.getAllergiesLower(camper);
-      const isAllergicToPeanuts: boolean = allergiesLower.includes('peanuts');
-
-      return { ...camper, isAllergicToPeanuts };
-    });
+    return camperDocuments.map(
+      (camper: CamperLeanDocument): CamperDto => this.mapToCamperDto(camper),
+    );
   }
 
+  /**
+   * Retrieves a single camper by ID.
+   *
+   * @param {string} id - The ID of the camper.
+   * @return {Promise<CamperDto>} A promise that resolves with the CamperDto object corresponding to the provided ID.
+   * @throws {NotFoundException} if the camper is not found.
+   */
   async findOneById(id: string): Promise<CamperDto> {
-    const camper: CamperLeanDocument | null = await this.camperModel
+    const camperDocument: CamperLeanDocument | null = await this.camperModel
       .findById(new ObjectId(id))
       .lean<CamperLeanDocument>();
 
-    if (!camper) {
+    if (!camperDocument) {
       throw new NotFoundException('Camper was not found.');
     }
 
+    return this.mapToCamperDto(camperDocument);
+  }
+
+  /**
+   * Maps a CamperLeanDocument object to a CamperDto object.
+   *
+   * @param {CamperLeanDocument} camper - The camper object to be transformed.
+   * @return {CamperDto} The transformed CamperDto object.
+   */
+  private mapToCamperDto(camper: CamperLeanDocument): CamperDto {
+    const normalizedAllergies: string[] = this.getNormalizedAllergies(camper);
+
     return {
       ...camper,
-      isAllergicToPeanuts: this.getAllergiesLower(camper).includes('peanuts'),
+      isAllergicToPeanuts: normalizedAllergies.includes('peanuts'),
     };
   }
 
-  private getAllergiesLower(camper: CamperLeanDocument): string[] {
+  /**
+   * Normalizes the list of allergies for a camper by converting each allergy to lowercase.
+   *
+   * @param camper - The camper object containing the allergies to be normalized.
+   * @return An array of normalized allergy strings in lowercase.
+   */
+  private getNormalizedAllergies(camper: CamperLeanDocument): string[] {
     return camper.allergies.map((allergy: string): string =>
       allergy.toLowerCase(),
     );
